9e14231ad8e5a367a7442c4e65f391ca034c1090
WJK08-star <wjk6@icloud.com>
2025-10-16 09:23:04 -0400

    Add files via upload

---
+++ Build_a_Dino.py
@@ -0,0 +1,47 @@
+# This program will use a dinosaur class to creat a dinosaur.
+
+class Dinosaur:
+
+  def __init__(self, name, species, diet, age):
+
+   self_name = name
+   self_species = species
+   self_diet = diet
+   self_age = age
+
+  # Dinosaur 1
+  dino_1_name = "Rex"
+  dino_1_species = "Tyranosaurus Rex"
+  dino_1_diet = "meat"
+  dino_1_age = 12
+
+  # Dinosaur 2
+  dino_2_name = "Bumpy"
+  dino_2_species = "Ankylosaurus"
+  dino_2_diet = "plants"
+  dino_2_age = 9
+
+  # Dinosaur 3
+  dino_3_name = "Spike"
+  dino_3_species = "Spinosaurus"
+  dino_3_diet = "fish"
+  dino_3_age = 15
+
+
+  print(f"{dino_2_name} lets out a mighty ROAR!")
+
+
+  print(f"{dino_1_name} is eating {dino_1_diet}.")
+
+
+  dino_1 = print("Name: Rex", " Species: Tyrannosaurus Rex", " Diet: meat", "Age: 12")
+  dino_2 = print("Name: Bumpy", "Species: Ankylosaurus", "Diet: plants", "Age 9")
+  dino_3 = print("Name: Spike", " Species: Spinosaurus", "Diet: fish", "Age 15")
+
+
+
+
+    
+  
+
+  
